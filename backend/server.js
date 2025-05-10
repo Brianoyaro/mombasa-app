0776007137853f11f889794afd3735ed753dc6fd
Brianoyaro <brianoyaro2000@yahoo.com>
2025-05-10 22:47:02 +0300

    removed duplicate line where I imported router twice

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = require('./src/routes');
-const router = require('./src/routes');
-
 const port = process.env.PORT || 3000;
 const app = express();
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { registerUser } = require('../controllers/authController');
 const router = express.Router();
 
 
