79992959472d5503b92a501cc6a2cb625647a483
Brianoyaro <brianoyaro2000@yahoo.com>
2025-06-11 10:05:39 +0300

    changed CORS allowed origin rom localhost to render supplied live URL

--- backend/backend.js
+++ backend/backend.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({ 
-  origin: "http://localhost:3000", // Replace with your frontend URL
+  origin: "https://mombasa-app-frontend.onrender.com/", //http://localhost:3000", // Replace with your frontend URL
   credentials: true, // Allow credentials (cookies, authorization headers, etc.)
  })); // Allow all origins
 app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
